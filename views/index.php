a9c2d92c961fbb7034ee49fb66783f8e1e36a4bd
Sunday Kodonon <kodonoumahuwanu@gmail.com>
2021-10-02 18:47:33 +0100

    replace pictures

--- views/index.php
+++ views/index.php
@@ -5,28 +5,28 @@
   <div class="row d-flex align-items-center">
 
     <!-- <div class="intro-text"> -->
-    <div class="intro-text">
+    <div class="intro-text swing font-weight-bold">
       <h2>Bienvenue à <span class="name">COMMUNICATION ET MEDIA CONSULTING</span></h2>
       <p>Votre guide dans le monde du média.</p>
       <!-- <a href="#about" class="btn-get-started scrollto">Get Started</a> -->
     </div>
 
     <!-- <div class="product-screens">-->
-    <div class="product-screens">
+    <!-- <div class="product-screens">
 
       <div class="product-screen-1 wow fadeInUp" data-wow-delay="0.4s" data-wow-duration="0.6s">
-        <img src="img/product-screen-1.png" alt="">
+        <img src="img/media/1.jpg" alt="">
       </div>
 
       <div class="product-screen-2 wow fadeInUp" data-wow-delay="0.2s" data-wow-duration="0.6s">
-        <img src="img/product-screen-2.png" alt="">
+        <img src="img/media/2.jpg" alt="">
       </div>
   
       <div class="product-screen-3 wow fadeInUp" data-wow-duration="0.6s">
-        <img src="img/product-screen-3.png" alt="">
+        <img src="img/media/3.jpg" alt="">
       </div>
 
-    </div>
+    </div> -->
   </div>
 
 </section><!-- #intro -->
